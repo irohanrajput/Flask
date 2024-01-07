148ecea434707dc5f73a60d33c7543282979e1e1
irohanrajput <itusharsingh27@gmail.com>
2024-01-08 01:49:34 +0530

    database setup

--- flaskCRUD/app.py
+++ flaskCRUD/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,15 +12,29 @@ class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def hello_world():
-    return render_template("index.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        if title and desc is not None:
+            todo = Todo(title = title, desc = desc)
+            db.session.add(todo)
+            db.session.commit()
+    showtodo = Todo.query.all()
+    return render_template("index.html", showtodo = showtodo)
+
+# @app.route("/show")
+# def show():
+#     allTodo = Todo.query.all()
+#     print(allTodo)
+#     return 
 
 
 if __name__ == "__main__":
